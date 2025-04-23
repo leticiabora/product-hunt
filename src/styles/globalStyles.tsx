5d88e5a245acd1801e8ae41e30b82885eda409a7
Leticia Bora <leticia.bora@gmail.com>
2025-04-23 09:50:56 -0300

    remove margin and padding

--- src/styles/globalStyles.tsx
+++ src/styles/globalStyles.tsx
@@ -1,11 +1,18 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   :root {
     --font-inter: 'Inter', sans-serif;
   }
 
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     font-family: var(--font-inter);
+    font-size: 16px;
+    margin: 0;
+    padding: 0;
   }
-`
+`;
