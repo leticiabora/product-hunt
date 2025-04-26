8b28ac33fe8c2475cb6916c6f0c2e2767db4eb09
Leticia Bora <leticia.bora@gmail.com>
2025-04-26 18:09:35 -0300

    add error and not found

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,43 @@
 'use client';
 
+import Button from '@/components/Button/Button';
+import { Container } from '@/components/Container/Container.styles';
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
+
+import styled from 'styled-components';
+
+const ErrorContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 400px;
+
+  @media (max-width: 768px) {
+    height: 250px;
+  }
+`;
+
 export default function Error() {
-  return <p>Something went wrong!</p>;
+  return (
+    <ErrorContainer>
+      <h1>Oh noes! Something went wrong.</h1>
+      <ImageContainer>
+        <Image
+          src="/product-hunt-cat.png"
+          alt="Product Hunt cat"
+          style={{ objectFit: 'contain' }}
+          fill
+          sizes="(max-width: 768px) 100vw, 800px"
+        />
+      </ImageContainer>
+      <Button onClick={() => redirect('/')}>Reload Page</Button>
+    </ErrorContainer>
+  );
 }
