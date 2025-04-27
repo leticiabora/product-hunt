7879f323c3fff4e640a9f74f9c817e756ec30cbc
Leticia Bora <leticia.bora@gmail.com>
2025-04-27 01:38:16 -0300

    fix loading

--- src/components/pages/Homepage/Loading/Loading.tsx
+++ src/components/pages/Homepage/Loading/Loading.tsx
@@ -2,21 +2,27 @@ import Skeleton, { SkeletonType } from '@/components/Skeleton/Skeleton';
 import * as S from '../Homepage.styles';
 
 export const Loading = () => {
-  return Array.from({ length: 10 }, (_, index) => (
-    <S.CardContainer key={index}>
-      <S.CardWrapper>
-        <S.Wrapper>
-          <Skeleton variant={SkeletonType.RECTANGULAR} width={60} height={60} />
-          <S.CardContent>
-            <S.Item>
-              <Skeleton width={150} />
-            </S.Item>
-            <S.Title>
-              <Skeleton width={300} />
-            </S.Title>
-          </S.CardContent>
-        </S.Wrapper>
-      </S.CardWrapper>
-    </S.CardContainer>
-  ));
+  return (
+    <S.ContainerList>
+        <S.TabContent>
+        {Array.from({ length: 10 }, (_, index) => (
+          <S.CardContainer key={index}>
+            <S.CardWrapper>
+              <S.Wrapper>
+                <Skeleton variant={SkeletonType.RECTANGULAR} width={60} height={60} />
+                <S.CardContent>
+                  <S.Item>
+                    <Skeleton width={150} />
+                  </S.Item>
+                  <S.Title>
+                    <Skeleton width={300} />
+                  </S.Title>
+                </S.CardContent>
+              </S.Wrapper>
+            </S.CardWrapper>
+          </S.CardContainer>
+        ))}
+    </S.TabContent>
+      </S.ContainerList>
+  );
 };
